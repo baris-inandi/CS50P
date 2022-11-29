923ebd105d177029fa14344158ac9d69b77967e7
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-11-29 16:23:13 +0000

    Nov 29, 2022, 4:23 PM

--- pset2/coke/coke.py
+++ pset2/coke/coke.py
@@ -1,9 +1,25 @@
 def valid_input(x):
+    # returns the string only if it is a valid denomination
     i = input(x)
     if i in ["25", "10", "5"]:
         return i
     return None
 
 
-while valid_input("Insert Coin: "):
-    pass
+def main():
+    # define initial price of 50 cents
+    due = 50
+    # ask for input for the first time
+    i = valid_input("Insert Coin: ")
+    while i:
+        due -= i
+        # ask for input
+        print(f"Amount due: {due}")
+        i = valid_input("Insert Coin: ")
+
+    if due < 0:
+        print(f"Change owed {-1*due}")
+
+
+if __name__ == "__main__":
+    main()
