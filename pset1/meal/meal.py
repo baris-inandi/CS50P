d15b92dec386d78c33b8088f7ead4e9e57f6bd41
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-11-29 14:21:53 +0000

    Tue, Nov 29, 2022, 2:21 PM +00:00

--- pset1/meal/meal.py
+++ pset1/meal/meal.py
@@ -3,7 +3,11 @@ def main():
 
 
 def convert(time):
-    ...
+    split = time.strip().split(":")
+    # sep
+    hours = int(split[0])
+    minutes = int(split[-1])
+
 
 
 if __name__ == "__main__":
