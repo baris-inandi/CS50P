bd31604e3ae5c8063f67be915629d950954e11e1
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-11-29 11:00:46 +0000

    Tue, Nov 29, 2022, 11:00 AM +00:00

--- pset1/deep/deep.py
+++ pset1/deep/deep.py
@@ -1,1 +1,5 @@
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if answer == "42" or answer.lower().replace(" ", "-") == "forty-two":
+    print("Yes")
+else:
+    print("No")
