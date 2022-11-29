5a55e8edca3646504fe5fe97f3cb452472e32305
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-11-29 16:17:07 +0000

    Nov 29, 2022, 4:17 PM

--- pset2/coke/coke.py
+++ pset2/coke/coke.py
@@ -1,6 +1,9 @@
-def valid_input():
-    pass
+def valid_input(x):
+    i = input(x)
+    if i in ["25", "10", "5"]:
+        return i
+    return None
 
 
-while valid_input():
+while valid_input("Insert Coin: "):
     pass
