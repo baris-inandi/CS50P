b2aead2b2e0230d7d251e249ea21db8a78b251fb
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-12-02 23:35:48 +0000

    fix twttr

--- pset2/twttr/twttr.py
+++ pset2/twttr/twttr.py
@@ -2,7 +2,7 @@ def main():
     vowels = ["a", "e", "i", "o", "u"]
     i = input("Input: ")
     # a list of characters where `c not in vowels`
-    print("".join(c for c in i if c not in vowels))
+    print("".join(c for c in i if c.lower() not in vowels))
 
 
 if __name__ == "__main__":
